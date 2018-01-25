aa45c6b263b38d7a18551252ae6f84c4392cbf03
Hung_Lin_Lee <killersbear@gmail.com>
2018-01-25 17:40:19 +0800

    111

--- src/main/java/org/iii/ee100/sample/hunglinlee/CalculateImpl.java
+++ src/main/java/org/iii/ee100/sample/hunglinlee/CalculateImpl.java
@@ -3,22 +3,18 @@ package org.iii.ee100.sample.hunglinlee;
 public class CalculateImpl implements org.iii.ee100.sample.Calculate {
 
 	public int add(int a, int b) {
-		// TODO Auto-generated method stub
 		return a+b;
 	}
 
 	public int sub(int a, int b) {
-		// TODO Auto-generated method stub
 		return a-b;
 	}
 
 	public int mul(int a, int b) {
-		// TODO Auto-generated method stub
 		return a*b;
 	}
 
 	public double div(int a, int b) {
-		// TODO Auto-generated method stub
 		return a/b;
 	}
 
