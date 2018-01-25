f5dbe4f39f03906dbd2b1b6afc5cbfafbb5d3e38
Gary Lee <jakarta99@gmail.com>
2018-01-25 16:36:22 +0800

    add ignore settting and calculate interface.

---
+++ src/main/java/org/iii/ee100/sample/Calculate.java
@@ -0,0 +1,13 @@
+package org.iii.ee100.sample;
+
+public interface Calculate {
+
+	public int add(int a, int b);
+	
+	public int sub(int a, int b);
+	
+	public int mul(int a, int b);
+	
+	public double div(int a, int b);
+	
+}
