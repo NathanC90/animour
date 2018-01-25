e59f24725e2b15b3071d2fe879b5452df1afae5b
Kevin Hsu <k5846yu@gmail.com>
2018-01-25 22:15:41 +0800

    push successful

---
+++ src/main/java/org/iii/ee100/sample/kevinhsu/CalculateImpl.java
@@ -0,0 +1,22 @@
+package org.iii.ee100.sample.kevinhsu;
+
+import org.iii.ee100.sample.Calculate;
+
+public class CalculateImpl implements Calculate {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+	public int sub(int a, int b) {
+		return a - b;
+	}
+
+	public int mul(int a, int b) {
+		return a * b;
+	}
+
+	public double div(int a, int b) {
+		return a / b;
+	}
+
+}
