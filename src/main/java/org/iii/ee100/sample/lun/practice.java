c01961225b41dd68ccfded7cbfaf5051ee503292
chung lun Hsiao <nbernie1003@gmail.com>
2018-01-25 17:41:01 +0800

    lunlun

---
+++ src/main/java/org/iii/ee100/sample/lun/practice.java
@@ -0,0 +1,27 @@
+package org.iii.ee100.sample.lun;
+
+import org.iii.ee100.sample.Calculate;
+
+public class practice implements Calculate {
+
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+	public int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+
+	public int mul(int a, int b) {
+		// TODO Auto-generated method stub
+		return a*b;
+	}
+
+	public double div(int a, int b) {
+		// TODO Auto-generated method stub
+		return a/b;
+	}
+
+}
