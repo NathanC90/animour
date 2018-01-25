469911f0036ddf128cb399109943498f0d7b7ec7
NathanC90 <35530607+NathanC90@users.noreply.github.com>
2018-01-25 17:07:57 +0800

    no message

--- src/main/java/org/iii/ee100/sample/nathan/TryCalculate.java
+++ src/main/java/org/iii/ee100/sample/nathan/TryCalculate.java
@@ -1,24 +1,28 @@
-package org.iii.ee100.sample.gary;
+package src.main.java.org.iii.ee100.sample.nathan;
 
-import org.iii.ee100.sample.Calculate;
+import src.main.java.org.iii.ee100.sample.Calculate;
 
-public class CalculateImpl implements Calculate {
+public class TryCalculate implements Calculate {
 
+	@Override
 	public int add(int a, int b) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	@Override
 	public int sub(int a, int b) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	@Override
 	public int mul(int a, int b) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	@Override
 	public double div(int a, int b) {
 		// TODO Auto-generated method stub
 		return 0;
