c03b983e4546ac0efa06e6f3627a09575b7c35e1
chung lun Hsiao <nbernie1003@gmail.com>
2018-02-05 22:50:18 +0800

    lun one

--- src/main/java/org/iii/ee100/sample/lun/CalculateImpl.java
+++ src/main/java/org/iii/ee100/sample/lun/CalculateImpl.java
@@ -2,7 +2,7 @@ package org.iii.ee100.sample.lun;
 
 import org.iii.ee100.sample.Calculate;
 
-public class CalculateImp implements Calculate {
+public class CalculateImpl implements Calculate {
 
 	public int add(int a, int c) {
 		return a+c;
--- src/main/java/org/iii/ee100/sample/lun/CalculateImplTest.java
+++ src/main/java/org/iii/ee100/sample/lun/CalculateImplTest.java
@@ -4,26 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class CalculateImpTest {
+public class CalculateImplTest {
 
 	@Test
 	public void testAdd() {
-		assertEquals(12,new CalculateImp().add(8, 4));
+		assertEquals(12,new CalculateImpl().add(8, 4));
 	}
 
 	@Test
 	public void testSub() {
-		assertEquals(5,new CalculateImp().sub(10, 5));
+		assertEquals(5,new CalculateImpl().sub(10, 5));
 	}
 
 	@Test
 	public void testMul() {
-		assertEquals(9,new CalculateImp().mul(3, 3));
+		assertEquals(9,new CalculateImpl().mul(3, 3));
 	}
 
 	@Test
 	public void testDiv() {
-		assertEquals(3,new CalculateImp().div(9, 3),0);
+		assertEquals(3,new CalculateImpl().div(9, 3),0);
 	}
 
 }
