c73182c1d27205c233eb9569259cffc906eacbe5
Kevin Hsu <k5846yu@gmail.com>
2018-02-13 19:53:50 +0800

    add hikari

--- src/main/java/org/iii/ee100/sample/sneakers/SneakersDao.java
+++ src/main/java/org/iii/ee100/sample/sneakers/SneakersDao.java
@@ -4,16 +4,34 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SneakersDao {
-	Connection conn;
-	PreparedStatement pstmt;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
-	// insert
+public class SneakersDao {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	
+	
+	private Connection getConnection() throws SQLException {
+		String connUrl = "jdbc:postgresql://localhost:5432/testdb";
+		String user = "postgres";
+		String pswd = "postgres";
+		
+		HikariConfig config = new HikariConfig();
+		config.setUsername(user);
+		config.setPassword(pswd);
+		config.addDataSourceProperty("cachePrepStmts", "true");
+		config.addDataSourceProperty("prepStmtCacheSize", "250");
+		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+		//Connection conn = DriverManager.getConnection(connUrl, "postgres", "postgres");
+		
+		HikariDataSource ds = new HikariDataSource(config);
+		return ds.getConnection();
+	}
+
+	// insert 
 	public void insert(Sneakers sn) {
 		try {
-			String connUrl = "jdbc:postgresql://localhost:5432/testdb";
-			conn = DriverManager.getConnection(connUrl, "postgres", "postgres");
-
 			String insql = "INSERT INTO sneakers (name, color, brand, price, launch) VALUES (?, ?, ?, ?, ?)";
 			pstmt = conn.prepareStatement(insql, Statement.RETURN_GENERATED_KEYS);
 
@@ -24,11 +42,11 @@ public class SneakersDao {
 			pstmt.setDate(5, sn.getLaunch());
 			pstmt.executeUpdate();
 
-			ResultSet rsKey = pstmt.getGeneratedKeys();
+			ResultSet rsKey= pstmt.getGeneratedKeys();
 			if (rsKey.next()) {
 				sn.setId(rsKey.getLong(1));
 			}
-
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
@@ -43,12 +61,9 @@ public class SneakersDao {
 		}
 	} // end of insert
 
-	// update
+	// update 
 	public void update(Sneakers sn) {
 		try {
-			String connUrl = "jdbc:postgresql://localhost:5432/testdb";
-			conn = DriverManager.getConnection(connUrl, "postgres", "postgres");
-
 			String upsql = "UPDATE sneakers SET name = ?, color = ?, brand = ? , price = ?, launch = ? WHERE id = ?";
 			pstmt = conn.prepareStatement(upsql);
 
@@ -78,9 +93,6 @@ public class SneakersDao {
 	// delete
 	public void delete(Long id) {
 		try {
-			String connUrl = "jdbc:postgresql://localhost:5432/testdb";
-			conn = DriverManager.getConnection(connUrl, "postgres", "postgres");
-
 			String dlsql = "DELETE FROM sneakers WHERE id = ?";
 			pstmt = conn.prepareStatement(dlsql);
 
@@ -108,23 +120,20 @@ public class SneakersDao {
 		ResultSet rs = null;
 
 		try {
-			String connUrl = "jdbc:postgresql://localhost:5432/testdb";
-			conn = DriverManager.getConnection(connUrl, "postgres", "postgres");
-
 			String slsql = "SELECT id, name, color, brand, price, launch FROM sneakers ORDER BY id";
 			pstmt = conn.prepareStatement(slsql);
 			rs = pstmt.executeQuery();
 
-			while (rs.next()) {
-				sn = new Sneakers();
+			while (rs.next()) { 
+				sn = new Sneakers(); 
 				sn.setId(rs.getLong("id"));
 				sn.setName(rs.getString("name"));
 				sn.setColor(rs.getString("color"));
 				sn.setBrand(rs.getString("brand"));
 				sn.setPrice(rs.getFloat("price"));
 				sn.setLaunch(rs.getDate("launch"));
-
-				snlist.add(sn);
+				
+				snlist.add(sn); 
 			} // end of while
 
 		} catch (SQLException e) {
@@ -150,9 +159,6 @@ public class SneakersDao {
 		ResultSet rs = null;
 
 		try {
-			String connUrl = "jdbc:postgresql://localhost:5432/testdb";
-			conn = DriverManager.getConnection(connUrl, "postgres", "postgres");
-
 			String slidsql = "SELECT id, name, color, brand, price, launch FROM sneakers WHERE id = ?";
 			pstmt = conn.prepareStatement(slidsql);
 			pstmt.setLong(1, id);
