f13f58d6b9df76c03cd04c52656414bdbaab3e73
NathanC90 <35530607+NathanC90@users.noreply.github.com>
2018-02-21 22:43:22 +0800

    update DAO

--- src/main/java/org/iii/ee100/sample/music/MusicDao.java
+++ src/main/java/org/iii/ee100/sample/music/MusicDao.java
@@ -9,6 +9,9 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class MusicDao{
 	private static final String INSERT_STMT = "INSERT INTO music (title, pname, genre, clicks, rdate) VALUES (?,?,?,?,?)";
 	private static final String UPDATE_STMT ="UPDATE music SET title=?, pname=?, genre=?, clicks=?, rdate=? WHERE id=?";
@@ -24,9 +27,18 @@ public class MusicDao{
 		String user ="postgres";
 		String password = "postgres";
 		
-		Connection conn = DriverManager.getConnection(connUrl, user, password);
-		
-		return conn;
+		//Connection conn = DriverManager.getConnection(connUrl, user, password);
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(connUrl);
+		config.setUsername(user);
+		config.setPassword(password);
+		config.addDataSourceProperty("cachePrepStmts", "true");
+		config.addDataSourceProperty("prepStmtCacheSize", "250");
+		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+		HikariDataSource ds = new HikariDataSource(config);
+
+		return ds.getConnection();
 	}
 	
 	//select all
