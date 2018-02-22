caf374741b274e386a471be8fa694c59ee106e39
Gary Lee <jakarta99@gmail.com>
2018-02-22 21:38:06 +0800

    index.jsp

--- src/main/java/org/iii/ee100/streetanimal/StreetAnimalApp.java
+++ src/main/java/org/iii/ee100/streetanimal/StreetAnimalApp.java
@@ -1,9 +1,9 @@
 package org.iii.ee100.streetanimal;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 public class StreetAnimalApp {
 
 	public static void main(String[] args) {
