dd16c17736b30a9c3ee0d7760345799a9a6da08c
MICK-ASUS-NB\Mick-NB <mickking122@gmail.com>
2018-03-01 21:01:46 +0800

    update

--- src/main/java/org/iii/ee100/streetanimal/MickController.java
+++ src/main/java/org/iii/ee100/streetanimal/MickController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MickController {
 	
-	@RequestMapping("/Mick")
+	@RequestMapping("/")
 	public String index() {
-		return "/index.jsp";
+		return "/mick.jsp";
 	}
+	
 }
