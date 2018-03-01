af9b8205f1e29f5b81ebd9332b3c1066b3460479
Hung_Lin_Lee <killersbear@gmail.com>
2018-03-01 20:46:28 +0800

    00

--- src/main/java/org/iii/ee100/streetanimal/LeeController.java
+++ src/main/java/org/iii/ee100/streetanimal/LeeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LeeController {
-	@RequestMapping("/")
+	@RequestMapping("/lee")
 	public String index() {
 		return "/lee.jsp";
 	}
