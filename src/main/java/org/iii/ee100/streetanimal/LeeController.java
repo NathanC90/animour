adeca602eac91fb37020a7c6cc29b348964f40d5
Hung_Lin_Lee <killersbear@gmail.com>
2018-03-01 20:40:46 +0800

    /

--- src/main/java/org/iii/ee100/streetanimal/LeeController.java
+++ src/main/java/org/iii/ee100/streetanimal/LeeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LeeController {
 	@RequestMapping("/")
 	public String index() {
-		return "index.jsp";
+		return "/index.jsp";
 	}
 	
 }
