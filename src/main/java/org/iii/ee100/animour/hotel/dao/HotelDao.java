324ece6eabdeb7776b9c5cd04decfbf10971e9b4
chung lun Hsiao <nbernie1003@gmail.com>
2018-03-16 19:44:10 +0800

    newupdate

---
+++ src/main/java/org/iii/ee100/animour/hotel/dao/HotelDao.java
@@ -0,0 +1,160 @@
+package org.iii.ee100.animour.hotel.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.iii.ee100.animour.hotel.entity.HotelBean;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class HotelDao {
+	private static final String InsertStmt = "INSERT INTO hotel ( hotelId, onwer, type, checkIn, total,species,dogName) VALUES ( ?, ?, ?, ?, ?,?)";
+	private static final String UpdateStmt = "UPDATE hotel SET  onwer=?, type=?, checkIn=?, total=?,species=?,dogName=? WHERE hotelId = ?";
+	private static final String DeleteStmt = "Delete from hotel WHERE hotelId = ?";
+	private static final String FindOneStmt = "SELECT hotelId, onwer, type, checkIn, total,species,dogName from hotel where hotelId=?";
+	private static final String FindAllStmt = "SELECT hotelId, onwer, type, checkIn, total,species,dogName from hotel order by hotelId";
+
+	
+	
+	private HikariDataSource getConnection() {
+		String connUrl = "jdbc:postgresql://localhost:5432/testdb";
+		String user = "postgres";
+		String password = "postgres";
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(connUrl);
+		config.setUsername(user);
+		config.setPassword(password);
+		config.addDataSourceProperty("cachePrepStmts", "true");
+		config.addDataSourceProperty("prepStmtCacheSize", "250");
+		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+		HikariDataSource ds = new HikariDataSource(config);
+		return ds;
+
+	}
+
+	public void insert(HotelBean hotel, java.util.Date checkIn) {
+		ResultSet rs = null;
+
+		try (HikariDataSource ds = getConnection();
+				Connection conn = ds.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(InsertStmt, Statement.RETURN_GENERATED_KEYS);) {
+
+			pstmt.setString(1, hotel.getOnwer());
+			pstmt.setString(2, hotel.getType());
+			pstmt.setInt(3, hotel.getTotal());
+			pstmt.setString(4, hotel.getSpecies());
+			pstmt.setString(5, hotel.getDogName());
+			Timestamp times = new Timestamp(System.currentTimeMillis());
+			hotel.setCheckIn(times);
+			pstmt.setTimestamp(6, hotel.getCheckIn());
+			pstmt.executeUpdate();
+			rs = pstmt.getGeneratedKeys();
+			if (rs.next()) {
+				hotel.setHotelId(rs.getLong(1));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void update(HotelBean customer) throws SQLException {
+		try (HikariDataSource ds = getConnection();
+				Connection conn = ds.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(UpdateStmt);) {
+			pstmt.setString(1, customer.getDogName());
+			pstmt.setString(2, customer.getOnwer());
+
+			pstmt.setTimestamp(3, customer.getCheckIn());
+			pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// ResultSet rs =pstmt.getGeneratedKeys();
+		// if(rs.next()) {
+		// customer.setCno(rs.getLong(1));
+		// }
+	}
+
+	public void delete(Long hotelId) throws SQLException {
+
+		try (HikariDataSource ds = getConnection();
+				Connection conn = ds.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(DeleteStmt);) {
+			pstmt.setLong(1, hotelId);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public HotelBean FindByCno(Long cno) throws SQLException {
+		HotelBean hotels = null;
+		ResultSet rs = null;
+		try (HikariDataSource ds = getConnection();
+				Connection conn = ds.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(FindOneStmt);) {
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				hotels = new HotelBean();
+				hotels.setHotelId(rs.getLong("hotelid"));
+				hotels.setDogName(rs.getString("dogname"));
+				hotels.setOnwer(rs.getString("onwer"));
+				hotels.setCheckIn(rs.getTimestamp("checkin"));
+				hotels.setTotal(rs.getInt("email"));
+				hotels.setSpecies(rs.getString("password"));
+				hotels.setType(rs.getString("type"));
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+		return hotels;
+	}
+
+	public List<HotelBean> getAll() throws SQLException {
+		HotelBean hotel = null;
+		List<HotelBean> hotels = new ArrayList<HotelBean>();
+		ResultSet rs = null;
+
+		try (HikariDataSource ds = getConnection();
+				Connection conn = ds.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(FindAllStmt);) {
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				hotel = new HotelBean();
+				hotel.setHotelId(rs.getLong("hotelid"));
+				hotel.setOnwer(rs.getString("onwer"));
+				hotel.setDogName(rs.getString("name"));
+				hotel.setSpecies(rs.getString("species"));
+				hotel.setTotal(rs.getInt("total"));
+				hotel.setType(rs.getString("type"));
+				hotel.setCheckIn(rs.getTimestamp("checkin"));
+				hotels.add(hotel);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+		return hotels;
+	}
+
+}
---
+++ src/main/java/org/iii/ee100/animour/hotel/entity/HotelBean.java
@@ -0,0 +1,68 @@
+package org.iii.ee100.animour.hotel.entity;
+
+public class HotelBean {
+	private Long hotelId;
+	private String onwer;
+	private String type;
+	private int total;
+	private String species;
+	private String dogName;
+	private java.sql.Timestamp checkIn;
+
+	public String getOnwer() {
+		return onwer;
+	}
+
+	public void setOnwer(String onwer) {
+		this.onwer = onwer;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public String getDogName() {
+		return dogName;
+	}
+
+	public void setDogName(String dogName) {
+		this.dogName = dogName;
+	}
+
+	public java.sql.Timestamp getCheckIn() {
+		return checkIn;
+	}
+
+	public void setCheckIn(java.sql.Timestamp checkIn) {
+		this.checkIn = checkIn;
+	}
+
+	public Long getHotelId() {
+		return hotelId;
+	}
+
+	public void setHotelId(Long hotelId) {
+		this.hotelId = hotelId;
+	}
+
+}
