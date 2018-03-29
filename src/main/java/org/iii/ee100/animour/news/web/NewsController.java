dec49ad7c0d4ccb23ddffe0101651c33119aaa76
NathanC90 <35530607+NathanC90@users.noreply.github.com>
2018-03-29 20:32:43 +0800

    update all

--- src/main/java/org/iii/ee100/animour/news/web/NewsController.java
+++ src/main/java/org/iii/ee100/animour/news/web/NewsController.java
@@ -1,6 +1,6 @@
 package org.iii.ee100.animour.news.web;
 
-import org.iii.ee100.animour.news.entity.NewsBean;
+import org.iii.ee100.animour.news.entity.News;
 import org.iii.ee100.animour.news.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,8 +20,8 @@ public class NewsController {
 	}
 	
 	@RequestMapping(path= {"/findOneNews"}, method={RequestMethod.GET})
-	public String findOne(NewsBean bean, Model model) {
-		NewsBean nb = newsService.getOne(Long.valueOf(bean.getSeqno()));
+	public String findOne(News bean, Model model) {
+		News nb = newsService.getOne(Long.valueOf(bean.getSeqno()));
 		if (nb != null) {
 			model.addAttribute("oneNews", nb);
 		}
@@ -35,21 +35,21 @@ public class NewsController {
 	}
 	
 	@RequestMapping(path= {"/deleteNews"}, method={RequestMethod.GET})
-	public String deleteNews(NewsBean bean, Model model) {
+	public String deleteNews(News bean, Model model) {
 		newsService.delete(bean.getSeqno());
 		model.addAttribute("delete", Long.valueOf(bean.getSeqno()));
 		return "/news/newsForm";
 	}
 	
 	@RequestMapping(path= {"/insertNews"}, method={RequestMethod.POST})
-	public String insertNews(NewsBean bean, Model model) {
+	public String insertNews(News bean, Model model) {
 		newsService.insert(bean);
 		model.addAttribute("insertNews", bean);
 		return "/news/newsForm";
 	}
 	
 	@RequestMapping(path= {"/updateNews"}, method={RequestMethod.GET})
-	public String updateNews(NewsBean bean, Model model) {
+	public String updateNews(News bean, Model model) {
 		newsService.update(bean);
 		model.addAttribute("updateNews", bean);
 		return "/news/newsForm";
