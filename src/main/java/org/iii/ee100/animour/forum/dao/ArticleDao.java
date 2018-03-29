93a231a7c2d99703a1c8c96179abc62a778f05e2
Hung_Lin_Lee <killersbear@gmail.com>
2018-03-29 20:51:47 +0800

    new ArticleDao

---
+++ src/main/java/org/iii/ee100/animour/forum/dao/ArticleDao.java
@@ -0,0 +1,8 @@
+package org.iii.ee100.animour.forum.dao;
+
+import org.iii.ee100.animour.forum.entity.Article;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArticleDao extends CrudRepository<Article, Long>{
+
+}
