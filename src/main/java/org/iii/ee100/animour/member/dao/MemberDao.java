950dad4cde8a1c2e305ab916f6faba5bc8988713
Lalalin <hsy9160@gmail.com>
2018-03-30 21:25:51 +0800

    +s

--- src/main/java/org/iii/ee100/animour/member/dao/MemberDao.java
+++ src/main/java/org/iii/ee100/animour/member/dao/MemberDao.java
@@ -1,9 +1,12 @@
 package org.iii.ee100.animour.member.dao;
 
+import javax.persistence.Id;
+
 import org.iii.ee100.animour.hospital.entity.Hospital;
 import org.iii.ee100.animour.member.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MemberDao extends CrudRepository<Member, Long>{
+	  void deleteById(Long id);
 
 }
