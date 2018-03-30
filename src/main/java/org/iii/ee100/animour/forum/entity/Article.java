e3f9746339b54d7f4f67a258dbd6f41f8894b576
lee <killersbear@gmail.com>
2018-03-30 18:47:10 +0800

    update Article.sql

--- src/main/java/org/iii/ee100/animour/forum/entity/Article.java
+++ src/main/java/org/iii/ee100/animour/forum/entity/Article.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -14,68 +13,90 @@ import org.iii.ee100.animour.member.entity.Member;
 @Entity
 @Table(name="ARTICLE")
 public class Article {
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="ID")
 	private Long id;
+	
 	@ManyToOne
 	private Category category;
+	
 	@Column(name="SUBJECT")
 	private String subject;
+	
 	@Column(name="CONTENT")
 	private String content;
+	
 	@Column(name="POST_TIME")
 	private java.sql.Timestamp postTime;
+	
 	@Column(name="UPDATE_TIME")
 	private java.sql.Timestamp updateTime;
+	
 	@ManyToOne
 //	@JoinColumn(name = "MEMBER_ID",referencedColumnName="ID")
 	private Member member;
+	
 	@Column(name="CLICK")
 	private Long click;
 	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getSubject() {
 		return subject;
 	}
+	
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
+	
 	public String getContent() {
 		return content;
 	}
+	
 	public void setContent(String content) {
 		this.content = content;
 	}
+	
 	public java.sql.Timestamp getPostTime() {
 		return postTime;
 	}
+	
 	public void setPostTime(java.sql.Timestamp postTime) {
 		this.postTime = postTime;
 	}
+	
 	public java.sql.Timestamp getUpdateTime() {
 		return updateTime;
 	}
+	
 	public void setUpdateTime(java.sql.Timestamp updateTime) {
 		this.updateTime = updateTime;
 	}
+	
 	public Member getMember() {
 		return member;
 	}
+	
 	public void setMember(Member member) {
 		this.member = member;
 	}
+	
 	public Long getClick() {
 		return click;
 	}
+	
 	public void setClick(Long click) {
 		this.click = click;
 	}
+	
 	@Override
 	public String toString() {
 		return "Article [id=" + id + ", subject=" + subject + ", content=" + content + ", postTime=" + postTime
--- src/main/resources/sql/Article.sql
+++ src/main/resources/sql/Article.sql
@@ -1,9 +1,10 @@
-drop table article;
-drop table member;
-
  INSERT INTO MEMBER(ACCOUNT,PASSWORD,NAME,NICK,CELL,EMAIL) 
  VALUES ('ViewSonic','123','Alex Wu','Alex','0900-000000','alex@gmail.com');
-insert into article (subject,content,post_Time,UPDATE_TIME,MEMBER_ID,CLICK) values ('aaaaa','aaaaaaaaaaaaaa','1999-01-08 04:05:06','1999-01-08 04:05:06',1,0);
+ 
+ INSERT INTO CATEGORY(NAME) 
+ VALUES ('PET');
+ 
+insert into article (subject,content,post_Time,UPDATE_TIME,MEMBER_ID,CLICK,CATEGORY_ID) values ('aaaaa','aaaaaaaaaaaaaa','1999-01-08 04:05:06','1999-01-08 04:05:06',1,0,1);
 
 select * from member;
 select * from article;
