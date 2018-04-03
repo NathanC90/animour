740c44997fbcd60838710050a7791503fcccc9b9
chung lun Hsiao <nbernie1003@gmail.com>
2018-04-03 22:52:23 +0800

    updadt new form

---
+++ src/main/java/org/iii/ee100/animour/salon/dao/SalonDao.java
@@ -0,0 +1,8 @@
+package org.iii.ee100.animour.salon.dao;
+
+import org.iii.ee100.animour.salon.entity.Salon;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SalonDao extends CrudRepository<Salon, Long> {
+
+}
---
+++ src/main/java/org/iii/ee100/animour/salon/entity/Salon.java
@@ -0,0 +1,116 @@
+package org.iii.ee100.animour.salon.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="salon")
+public class Salon {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
+	private Long id;
+	
+	@Column(name="RESERVATION")
+	private java.sql.Date reservation;
+	
+	@Column(name="OWNER")
+	private String owner;
+	
+	@Column(name="PHONE")
+	private int phone;
+	
+	@Column(name="DOG_NAME")
+	private String dogName;
+	
+	@Column(name="TYPE")
+	private String type;
+	
+	@Column(name="GENDER")
+	private String gender;
+	
+	@Column(name="FINISH_DATE")	
+	private java.sql.Date finishDate;
+	
+	@Column(name="SPENDING")
+    private int spending;
+	
+	@Column(name="DESIGNER")
+    private String designer;
+	
+	@Column(name="MEMBERID")
+    private Long memberId;
+	
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public java.sql.Date getReservation() {
+		return reservation;
+	}
+	public void setReservation(java.sql.Date reservation) {
+		this.reservation = reservation;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getDogName() {
+		return dogName;
+	}
+	public void setDogName(String dogName) {
+		this.dogName = dogName;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public java.sql.Date getFinishDate() {
+		return finishDate;
+	}
+	public void setFinishDate(java.sql.Date finishDate) {
+		this.finishDate = finishDate;
+	}
+	public int getSpending() {
+		return spending;
+	}
+	public void setSpending(int spending) {
+		this.spending = spending;
+	}
+	public String getDesigner() {
+		return designer;
+	}
+	public void setDesigner(String designer) {
+		this.designer = designer;
+	}
+	public Long getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(Long memberId) {
+		this.memberId = memberId;
+	}
+}
