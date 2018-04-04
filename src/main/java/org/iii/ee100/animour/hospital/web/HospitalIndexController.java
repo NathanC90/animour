b9f7a46a43033d7a6f7714eb15f4fdc60b119627
Lala <hsy9160@gmail.com>
2018-04-04 21:47:24 +0800

    update hospitalcontroller

--- src/main/java/org/iii/ee100/animour/hospital/web/HospitalIndexController.java
+++ src/main/java/org/iii/ee100/animour/hospital/web/HospitalIndexController.java
@@ -1,13 +1,23 @@
 package org.iii.ee100.animour.hospital.web;
 
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
+import org.iii.ee100.animour.hospital.entity.Appointment;
 import org.iii.ee100.animour.hospital.entity.Hospital;
+import org.iii.ee100.animour.hospital.service.AppointmentService;
 import org.iii.ee100.animour.hospital.service.HospitalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HospitalIndexController {
@@ -23,9 +33,49 @@ public class HospitalIndexController {
 		model.addAttribute("alllist", hospital);		
 		return "/hospital/hospitalindex";
 	}
-	
-	
 
+	@RequestMapping(path= {"/hospital/appointmentdate"},method={RequestMethod.POST})
+	public String order(Model model, Hospital hospital) {
+		if(hospital !=null) {
+		hospital =hospitalService.getOne(hospital.getId());
+		model.addAttribute("onehospital", hospital);
+		return "/hospital/appointment";
+		
+		}
+		else {
+			return "/news/NewsIndex";
+		}
+		}
+
+	@Autowired
+	AppointmentService appointmentService;	
+	
+	@RequestMapping("/insert_app_date")
+	public String order(Model model,Appointment appointment
+			,Hospital hospital
+			) {
+		appointmentService.insertAppoint(appointment);
+		if(appointment.getId() !=null) {
+		appointment=appointmentService.getOne(appointment.getId());
+		hospital=hospitalService.getOne(appointment.getHospital_id());
+		model.addAttribute("inserta",appointment );
+		model.addAttribute("hospital_insertApp",hospital );
+
+		}
+		return "/hospital/appointmentList";
+	}
+	
+	@RequestMapping("/findOrder")
+	public String order(Model model
+			) {
+		Iterable<Appointment> allorder=appointmentService.getAll();
+		model.addAttribute("app", allorder);
+		
+		return "/hospital/allappointment";
+	}
+	
+	
+    final static SimpleDateFormat  df = new SimpleDateFormat("yyyy-MM-dd hh:mm");
 	
 	
 }
