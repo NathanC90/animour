c07b775dbf16479cfa20bec0678ecbcf113b5af8
chung lun Hsiao <nbernie1003@gmail.com>
2018-04-08 10:39:34 +0800

    addnewservice

---
+++ src/main/java/org/iii/ee100/animour/salon/service/SalonService.java
@@ -0,0 +1,16 @@
+package org.iii.ee100.animour.salon.service;
+
+import org.iii.ee100.animour.salon.dao.SalonDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SalonService {
+	
+	@Autowired
+	private SalonDao salonDao;
+	
+	
+	
+
+}
