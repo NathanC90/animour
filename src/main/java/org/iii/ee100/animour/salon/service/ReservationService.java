434460a120d9181c1952246a148613da111e3ad0
chung lun Hsiao <nbernie1003@gmail.com>
2018-04-12 14:54:42 +0800

    fix myviews

--- src/main/java/org/iii/ee100/animour/salon/service/ReservationService.java
+++ src/main/java/org/iii/ee100/animour/salon/service/ReservationService.java
@@ -3,7 +3,9 @@ package org.iii.ee100.animour.salon.service;
 import java.util.List;
 
 import org.assertj.core.util.Lists;
+import org.iii.ee100.animour.salon.dao.DesignerDao;
 import org.iii.ee100.animour.salon.dao.ReservationDao;
+import org.iii.ee100.animour.salon.entity.Designer;
 import org.iii.ee100.animour.salon.entity.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +16,16 @@ public class ReservationService {
 	@Autowired
 	ReservationDao reservationDao;
 	
+	@Autowired
+	DesignerDao designerDao;
+	
 	public List<Reservation> getAll() {
 		return Lists.newArrayList(reservationDao.findAll());
+	}
+	
+	public List<Designer> getAllFreeTime(){
+		return Lists.newArrayList(designerDao.findAll());
+		
 	}
 
 }
