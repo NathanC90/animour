89374d21b5ef5c71f699ca8d63af212fadb70590
NathanC90 <35530607+NathanC90@users.noreply.github.com>
2018-04-26 00:16:19 +0800

    add annotation @Data to NewsEntity

--- src/main/java/org/iii/ee100/animour/news/entity/Event.java
+++ src/main/java/org/iii/ee100/animour/news/entity/Event.java
@@ -7,8 +7,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
 @Table(name="Event")
+@Data
 public class Event {
 
 	@Id
