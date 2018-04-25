4e137b1f694dbade42c4a9ef725fd85534c1ba9a
Lalalin <hsy9160@gmail.com>
2018-04-25 17:07:34 +0800

    modify Member.sql  and everyone.sql( 新增資料)

--- src/main/resources/sql/member.sql
+++ src/main/resources/sql/member.sql
@@ -12,7 +12,13 @@ Member
  ); 
  
  
- INSERT INTO MEMBER(ACCOUNT,PASSWORD,NAME,Nickname,CELL,EMAIL) 
- VALUES ('ViewSonic','123','Alex Wu','Alex','0900-000000','alex@gmail.com');
+ INSERT INTO MEMBER(ACCOUNT,PASSWORD,NAME,Nickname,CELL,EMAIL,Address) 
+ VALUES ('ViewSonic','123','Alex Wu','Alex','0900-000000','alex@gmail.com','台北市大安區');
+ INSERT INTO MEMBER(ACCOUNT,PASSWORD,NAME,Nickname,CELL,EMAIL,Address) 
+ VALUES ('BenQ','123','Annie Wu','Annie','0900-000000','annie@gmail.com','台北市中正區');
+ INSERT INTO MEMBER(ACCOUNT,PASSWORD,NAME,Nickname,CELL,EMAIL,Address) 
+ VALUES ('Sony','123','Sony Su','Apple','0900-000000','apple@gmail.com','台北市萬華區');
+ INSERT INTO MEMBER(ACCOUNT,PASSWORD,NAME) 
+ VALUES ('Apple','123','Apple Wu','Apple','0900-000000','apple@gmail.com','台北市內湖區');
  INSERT INTO MEMBER(ACCOUNT,PASSWORD,NAME) 
  VALUES ('Admin','123','管理員');
