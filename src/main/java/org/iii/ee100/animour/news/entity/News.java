34e139d200d7dedee3084c8a4feed7592e3275ea
NathanC90 <35530607+NathanC90@users.noreply.github.com>
2018-05-03 10:55:47 +0800

    add column PageView

--- src/main/java/org/iii/ee100/animour/news/entity/News.java
+++ src/main/java/org/iii/ee100/animour/news/entity/News.java
@@ -38,6 +38,8 @@ public class News {
 	private String updateUser;
 	@Column(name="UPDATE_TIME")
 	private java.sql.Timestamp updateTime;
+	@Column(name="PAGE_VIEW")
+	private int pageView;
 	@Column(name="IS_DEL")
 	private String isDel;
 	@Column(name="IS_ON")
@@ -115,6 +117,13 @@ public class News {
 	}
 	public void setIsOn(String isOn) {
 		this.isOn = isOn;
+	}
+	
+	public int getPageView() {
+		return pageView;
+	}
+	public void PageView(int pageView) {
+		this.pageView = pageView;
 	}
 
 }
