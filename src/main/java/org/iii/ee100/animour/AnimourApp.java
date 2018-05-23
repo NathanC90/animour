eb77a3962256ab9989f0afd35e03550617cbc63c
Hung_Lin_Lee <killersbear@gmail.com>
2018-05-23 15:30:46 +0800

    XSS攻擊防範

--- src/main/java/org/iii/ee100/animour/AnimourApp.java
+++ src/main/java/org/iii/ee100/animour/AnimourApp.java
@@ -2,7 +2,9 @@ package org.iii.ee100.animour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class AnimourApp {
 
