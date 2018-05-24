01602d76afafd2ecdc640f05f9d892b4ce6e3d16
蕭仲倫 <nbernie1003@gmail.com>
2018-05-24 11:46:25 +0800

    configrate

--- src/main/java/org/iii/ee100/animour/AnimourApp.java
+++ src/main/java/org/iii/ee100/animour/AnimourApp.java
@@ -2,14 +2,21 @@ package org.iii.ee100.animour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @ServletComponentScan
 @SpringBootApplication
-public class AnimourApp {
+public class AnimourApp extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AnimourApp.class);
+	}
+
+	public static void main(String[] args) throws Exception{
 		SpringApplication.run(AnimourApp.class, args);
 	}
 
